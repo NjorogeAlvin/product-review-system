e60b9dca7f33b92883bc8924b77a734816db5a1f
Alvin <alvinkiarie09@gmail.com>
2026-07-26 14:36:36 +0300

    Connect to the actual dB

--- admin/moderate.php
+++ admin/moderate.php
@@ -1,3 +1,56 @@
 <?php
+require_once __DIR__ . '/../config/db.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moderate</title>
+    <link rel="stylesheet" href="/Projects/product-review-system/assets/moderate.css">
+</head>
+<body>
+    <h1>Review Moderation</h1>
+    <nav>
+        <ul>
+            <li><a href="products.php">Products</a></li>
+            <li><a class ="active" href="moderate.php">Moderate</a></li>
+        </ul>
+    </nav>
+        <div id="table">
+        <table border="1">
+            <caption>Modeartion</caption>
+            <thead>
+                <tr>
+                    <th>Review ID</th>
+                    <th>Product ID</th>
+                    <th>User ID</th>
+                    <th>Title</th>
+                    <th>Ratings</th>
+                    <th>Comment</th>
+                    <th>Status</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
+
+<?php
 
 ?>
--- admin/products.php
+++ admin/products.php
@@ -1,3 +1,56 @@
 <?php
+require_once __DIR__ . '/../config/db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+    <link rel="stylesheet" href="/Projects/product-review-system/assets/products.css">
+</head>
+<body>
+    <h1>Products Management</h1>
+    <div id="nav_bar_products">
+        <nav>
+            <ul>
+                <li><a class="active" href="products.php">Products</a></li>
+                <li><a href="moderate.php">Moderate</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div id="add_buttons">
+        <button>Add Products</button>
+    </div>
+    <div id="table">
+        <table border="1">
+            <caption>Products</caption>
+            <thead>
+                <div id="product_head">
+                    <tr>
+                        <th>Product ID</th>
+                        <th>Name</th>
+                        <th>Brand</th>
+                        <th>Price</th>
+                        <th>Specs</th>
+                    </tr>
+                </div>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
+
+<?php
 
 ?>
